20f3ba1b844a625562b1bcbbaa4ea0b87e213239
Manjula Dube <dube.manjula668@gmail.com>
2018-03-02 09:25:16 +0530

    Serve index page on wire to client on port 3000

---
+++ index.js
@@ -0,0 +1,26 @@
+// Initialize express server
+var express = require('express');
+var app = express();
+
+// Read port from env loaded or by default port 3000
+var PORT = process.env.PORT || 3000;
+
+// To serve static assets mount directory 
+app.use(express.static(__dirname + "/public"));
+
+// Serve index.html from the mounted public directory
+app.get('/', function(req, res){ 
+
+    // Send index file on wire
+    res.sendFile('index.html');
+});
+
+// Boot server
+var server = app.listen(PORT, function () {
+ 
+  var host = server.address().address;
+  var port = server.address().port;
+ 
+  console.log('App running at http://%s:%s', host, port);
+ 
+});
