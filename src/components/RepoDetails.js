28ae85a16c0a50d70da3e48a9e77ca01a3007823
Manjula Dube <dube.manjula668@gmail.com>
2018-03-02 11:34:50 +0530

    add none check

--- src/components/RepoDetails.js
+++ src/components/RepoDetails.js
@@ -25,8 +25,8 @@ function RepoListElement(){
           onClick={() => window.open(repo.html_url, '_blank')}
           >
           <h3 className='blueText'>{repo.name}</h3>
-          <p> Language:
-              {repo.language !== null ? <span className='greenText'> {repo.language}</span> : <span className='redText'> Unknown </span>}
+          <p> Name:
+              {repo.name !== null ? <span className='greenText'> {repo.name}</span> : <span className='redText'> Unknown </span>}
           </p>
           <p>Description:</p>
           {repo.description !== null ? <span className='greenText'> {repo.description}</span> : <span className='redText'> None </span>}
