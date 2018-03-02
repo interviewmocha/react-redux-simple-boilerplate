c5b74e63cb9a85535a4519006ec232edcee5a6ef
Manjula Dube <dube.manjula668@gmail.com>
2018-03-02 11:32:34 +0530

    add css

--- src/components/RepoDetails.js
+++ src/components/RepoDetails.js
@@ -9,13 +9,33 @@ const RepoDetails = ({ repoDetail }) => {
         >
         <img src={repoDetail.avatar} alt={repoDetail.avatar}/>
         <h3 className='blueText'>{repo.name}</h3>
-        <p> Language:
-            
-        </p>
-        <p>Descriptions:</p>
+        {RepoListElement(repoDetail.repos)}
 
       </li>
     )
 }
+
+function RepoListElement(){
+  const RepoListElement = ({ repos }) => {
+    return _.map(repos, repo => {
+      return (
+        <li
+          key={repo.id}
+          className='list-group-item'
+          onClick={() => window.open(repo.html_url, '_blank')}
+          >
+          <h3 className='blueText'>{repo.name}</h3>
+          <p> Language:
+              {repo.language !== null ? <span className='greenText'> {repo.language}</span> : <span className='redText'> Unknown </span>}
+          </p>
+          <p>Description:</p>
+          {repo.description !== null ? <span className='greenText'> {repo.description}</span> : <span className='redText'> None </span>}
+        </li>
+      )
+    })
+  }
+}
+
+
 
 export default RepoDetails
